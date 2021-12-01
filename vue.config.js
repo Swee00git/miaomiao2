655045748f3814f8a4a17f4b01e5363b43338c98
Sweeoogit <xiaomei_0818@qq.com>
2021-12-01 22:26:39 +0800

    bug clear

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,18 @@
-/*module.exports = {
+module.exports = {
     devServer : {
         Proxy : {
-            /*'/api' : {
-                target : 'https://v0.yiketianqi.com',
+            '/api' : {
+                target : 'https://test-seo.edatastore.io',
                 changeOrigin : true
             }
-        }
+        },
+        host:'0.0.0.0',
+        public:'172.290.3.3:8080',
+        port:8080,
+        https:false,
+        hotOnly:false,
+        disableHostCheck:true,
+        open:true
     }
-}*/
+  }
 
